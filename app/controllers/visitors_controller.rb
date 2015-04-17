0228bcda13b62204cd86257f6b9f315de9707780
Mateusz Dobek <mat.dobek@gmail.com>
2015-04-17 18:56:32 +0200

    choose cities

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,12 @@
 class VisitorsController < ApplicationController
   def index
     #UpdateFoodtrucksLocation.new.call
+    trucks = params['city'] ? Foodtruck.where(city: params['city']) : Foodtruck.all
+
+    trucks = trucks.map do |f|
+      "{ lat: #{f.latitude}, lng: #{f.longitude}, infowindow: \"#{f.name}\" }"
+    end.join(',')
+    @foodtrucks = "[#{trucks}]"
+
   end
 end
