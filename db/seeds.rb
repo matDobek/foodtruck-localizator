41ffb3693001be00080a72cbb969952e81bcdd63
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2015-04-17 16:53:18 +0200

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
-Foodtruck.find_or_create_by(name: 'Carmnik', endpoint_data: 'CarmnikPl', city: 'Gdańsk')
-Foodtruck.find_or_create_by(name: 'Surf Burrger', endpoint_data: 'falatejstu', city: 'Gdańsk')
+Foodtruck.find_or_create_by(
+  name: 'Carmnik',
+  endpoint_data: 'CarmnikPl',
+  city: 'Gdańsk',
+  endpoint_type: :facebook_wall
+)
+Foodtruck.find_or_create_by(
+  name: 'Surf Burrger',
+  endpoint_data: 'falatejstu',
+  city: 'Gdańsk',
+  endpoint_type: :facebook_wall
+)
